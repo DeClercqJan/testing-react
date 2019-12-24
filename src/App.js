9e96525c8b6ce852acb2b85279571d9a3dcccb34
jan <declercqjan@gmail.com>
2019-12-24 12:18:09 +0100

    submit form gedaan

--- src/App.js
+++ src/App.js
@@ -1,50 +1,38 @@
 import React, { Component } from "react";
 import Table from "./Table";
+import Form from "./Form";
 
 class App extends Component {
-    state = {
-      characters: [
-        {
-          name: "Charlie",
-          job: "Janitor"
-        },
-        {
-          name: "Mac",
-          job: "Bouncer"
-        },
-        {
-          name: "Dee",
-          job: "Aspring actress"
-        },
-        {
-          name: "Dennis",
-          job: "Bartender"
-        }
-      ]
-    };
+  state = {
+    characters: [{name: "jan", job: "huisman"}]
+  };
 
-    removeCharacter = index => {
-      const { characters } = this.state;
+  removeCharacter = index => {
+    const { characters } = this.state;
 
-      this.setState({
-        characters: characters.filter((character, i) => {
-          return i !== index;
-        })
-      });
-    };
+    this.setState({
+      characters: characters.filter((character, i) => {
+        return i !== index;
+      })
+    });
+  };
 
-    render() {
-      const { characters } = this.state
-      
+  render() {
+    const { characters } = this.state;
+
     return (
       <div className="container">
         <Table
           characterData={characters}
           removeCharacter={this.removeCharacter}
         />
-      </div>
+        <Form handleSubmit={this.handleSubmit} />
+       </div>
     );
   }
+  handleSubmit = character => {
+    this.setState({ characters: [...this.state.characters, character] });
+  };
 }
 
 export default App;
