f963f26a57eeb0aec71ff6e419965f24e0ced3e9
jan <declercqjan@gmail.com>
2019-12-24 14:11:13 +0100

    missed some styling elements. Fixed it

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Form from "./Form";
 
 class App extends Component {
   state = {
-    characters: [{name: "jan", job: "huisman"}]
+    characters: [{ name: "jan", job: "huisman" }]
   };
 
   removeCharacter = index => {
@@ -22,12 +22,20 @@ class App extends Component {
 
     return (
       <div className="container">
+        <h1>First time checking out react.js</h1>
+        <p>
+          guide:
+          <a href="https://www.taniarascia.com/getting-started-with-react/">
+            https://www.taniarascia.com/getting-started-with-react/
+          </a>
+        </p>
+        <p>Add a character with a name and a job to the table.</p>
         <Table
           characterData={characters}
           removeCharacter={this.removeCharacter}
         />
         <Form handleSubmit={this.handleSubmit} />
-       </div>
+      </div>
     );
   }
   handleSubmit = character => {
